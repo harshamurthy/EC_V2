23a46c438157c61fa71340c0df8ad8dd7dc32c20
Jack Mallers <jimmymowschess@gmail.com>
2014-02-09 22:09:03 -0600

    Got everything back to normal

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -14,6 +14,7 @@ class ClientsController < ApplicationController
   # GET /clients/1
   # GET /clients/1.json
   def show
+    @client_chart = @client.sessions.group_by { |t| t.created_at }
   end
 
   # GET /clients/new
