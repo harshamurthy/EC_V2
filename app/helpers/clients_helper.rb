2ef32f77d06567697aaa48b39f03068a9e6e6a5f
Jack Mallers <jimmymowschess@gmail.com>
2014-02-11 14:12:11 -0600

    working on graph

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -1,2 +1,23 @@
 module ClientsHelper
+  def session_graph_data(client)
+    # Exercise.all.each do ||
+    (4.weeks.ago.to_date..Date.today).map do |date|
+      {
+        updated_at: date
+        triceps: client.crazy_method('triceps', date)
+        triceps: client.crazy_method('triceps', date)
+        triceps: client.crazy_method('triceps', date)
+        triceps: client.crazy_method('triceps', date)
+        triceps: client.crazy_method('triceps', date)
+        triceps: client.crazy_method('triceps', date)
+        triceps: client.crazy_method('triceps', date)
+        triceps: client.crazy_method('triceps', date)
+        triceps: client.crazy_method('triceps', date)
+        triceps: client.crazy_method('triceps', date)
+        triceps: client.crazy_method('triceps', date)
+        triceps: client.crazy_method('triceps', date)
+        triceps: client.crazy_method('triceps', date)
+      }
+    end
+  end
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -2,8 +2,15 @@ class Client < ActiveRecord::Base
   belongs_to :gym
   has_many :sessions
   has_many :setting_executions
+  has_many :exercise_executions, through: :sessions
 
   def name
     self.first_name + " " + self.last_name
+  end
+
+  def get_weight_for_exercise_and_day(exercise_name, date)
+    # assumes 1 exercise 1 day
+    exercise = Exercise.find_by_name(exercise_name)
+    self.exercise_executions.where(exercise_id: exercise.id).select{|ee| ee.update_at.to_date == date}.first.weight
   end
 end
