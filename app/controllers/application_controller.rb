fd5a9a70b39b2b19e9402aaf71b1ed48d8a7d50c
Jack Mallers <jimmymowschess@gmail.com>
2014-02-27 21:09:00 -0600

    fixing timezone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
   before_filter :authenticate_gym!
+  before_filter :set_timezone
 
   protected
+
+
+def set_timezone
+  Time.zone = 'Central Time (US & Canada)'
+end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [:location, :email]
