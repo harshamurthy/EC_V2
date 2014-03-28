4d4d2c4d1b4a7f921498bb629f4014680cd0631d
Jack Mallers <jimmymowschess@gmail.com>
2014-03-28 18:26:48 -0500

    pushing to heroku

--- app/controllers/fitbit_controller.rb
+++ app/controllers/fitbit_controller.rb
@@ -1,7 +1,7 @@
 class FitbitController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
-    client = Client.find_by_first_name("Jack") #really find the client by the id passed which will be the current_client once they can sign in
+    client = Client.find_by_first_name("Harsha") #really find the client by the id passed which will be the current_client once they can sign in
     @c = client
     @c.oauth_token = auth["credentials"]["token"]
     @c.oauth_secret = auth["credentials"]["secret"]
